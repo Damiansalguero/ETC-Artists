6d45bee014e39cabdab00eb6d15bf35f26e482ba
Damiansalguero <damian.salguero@aikq.de>
2020-07-24 09:48:40 +0200

    Update ejs, project structure

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-  res.render("index");
+  res.render("landing");
 });
 
 app.listen(8080, function () {
